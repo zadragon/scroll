8511d8ccc0fe7353a1ae19196934662cc75b4e17
zadragon <mase0804@naver.com>
2022-09-07 22:22:56 +0900

    2번째 커밋

---
+++ jsme/main.js
@@ -0,0 +1,48 @@
+(() => {
+    const sceneInfo = [
+        {
+            //0
+            heightNum: 5, //브라우저 높이의 5배로 scrollHeight 세팅
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector("#scroll-section-0"),
+            },
+        },
+        {
+            //1
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector("#scroll-section-1"),
+            },
+        },
+        {
+            //2
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector("#scroll-section-2"),
+            },
+        },
+        {
+            //3
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector("#scroll-section-3"),
+            },
+        },
+    ];
+
+    function setLayout() {
+        //각 스크롤 섹션의 높이 세팅
+        for (let i = 0; i < sceneInfo.length; i++) {
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+
+        console.log(sceneInfo);
+    }
+    window.addEventListener("resize", () => setLayout());
+    setLayout();
+})();
