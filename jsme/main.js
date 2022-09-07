f12a62e029b114cffd93911e0a668d55783de60c
zadragon <mase0804@naver.com>
2022-09-07 22:23:36 +0900

    2번째 커밋 테스트

--- jsme/main.js
+++ jsme/main.js
@@ -35,7 +35,7 @@
     ];
 
     function setLayout() {
-        //각 스크롤 섹션의 높이 세팅
+        //각 스크롤 섹션의 높이 세팅2
         for (let i = 0; i < sceneInfo.length; i++) {
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
